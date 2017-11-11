54cd6ebaef02d511f86b5a1ea89f193f68096abe
Caitlin Trussell <caitlintrussell@gmail.com>
2017-11-11 20:02:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,6 +12,6 @@ function destructivelyRemoveFirstDriver(){
   drivers.shift();
 }
 function appendDriver(name){
-  const drivers = ['Milo', 'Otis', 'Garfield']
-  const moreDrivers = [...drivers, 'Broom']
+  const drivers = ['Milo', 'Otis', 'Garfield'];
+  const moreDrivers = [...drivers, 'Broom'];
 }
